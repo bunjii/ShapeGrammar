9b5fba6482ce79ddcc581d5fd3e7618a3f361702
B. Izumi <bunji.izumi@gmail.com>
2023-08-28 11:32:48 +0200

    bug fix autorule 3

--- Classes/Rules/SG_AutoRule03.cs
+++ Classes/Rules/SG_AutoRule03.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Grasshopper.Kernel;
+using Rhino;
 using Rhino.Geometry;
 using Rhino.Geometry.Intersect;
 
@@ -40,24 +41,30 @@ namespace ShapeGrammar.Classes.Rules
         public override string RuleOperation(ref SG_Shape ss_ref, ref SG_Genotype gt)
         {
 
+
+            // collect R2 elements
             var selElems = ss_ref.Elems.Where(e => e.Autorule == 2);
             
+            // create node lists separately for above and below R0/R1 elements 
+            // these should exclude the supported nodes
             List<SG_Node> nds_side0 = selElems.Where(e => (e.Nodes[1].Pt.Z - e.Nodes[0].Pt.Z) > 0).Select(e => e.Nodes[1]).ToList();
             List<SG_Node> nds_side1 = selElems.Where(e => (e.Nodes[1].Pt.Z - e.Nodes[0].Pt.Z) < 0).Select(e => e.Nodes[1]).ToList();
 
+            // additioally add supported nodes to each list. 
             List<SG_Node> supNds = ss_ref.Nodes.Where(n => n.Support.SupportCondition > 0).ToList();
-
             foreach (var sn in supNds)
             {
                 var r2Elem_from_supNds = sn.Elements.Where(e => e.Autorule == 2);
 
-                if (r2Elem_from_supNds.Count() == 0)
+                if (r2Elem_from_supNds.Count() == 0) // case with no r2 elems on the support pts
                 {
                     nds_side0.Add(sn);
                     nds_side1.Add(sn);
                 }
 
-                else
+                else 
+                // in case there is a R2 elem from the support
+                // the node should be added only in the list opposite side of the R2 member. 
                 {
                     var r2_elem = r2Elem_from_supNds.First();
 
@@ -85,16 +92,6 @@ namespace ShapeGrammar.Classes.Rules
                     continue;
                 }
 
-                if (Math.Abs(nd0.Pt.X - nd1.Pt.X) < UT.PRES)
-                {
-                    if (i == nds_side0_sorted.Count - 2) continue;
-                    else
-                    {
-                        nd1 = nds_side0_sorted[i + 2];
-                        i++;
-                    }
-                }
-                
                 SG_Node[] nds = new SG_Node[2] { nd0, nd1 };
                 SG_Elem1D newElem = new SG_Elem1D(nds, -999, ElemName) { Autorule = 3};
 
@@ -111,60 +108,97 @@ namespace ShapeGrammar.Classes.Rules
                     continue;
                 }
 
-                if (Math.Abs(nd0.Pt.X - nd1.Pt.X) < UT.PRES)
-                {
-                    if (i == nds_side1_sorted.Count - 2) continue;
-                    else
-                    {
-                        nd1 = nds_side1_sorted[i + 2];
-                        i++;
-                    }
-                }
-
                 SG_Node[] nds = new SG_Node[2] { nd0, nd1 };
                 SG_Elem1D newElem = new SG_Elem1D(nds, -999, ElemName) { Autorule = 3};
 
                 ss_ref.AddNewElement(newElem);
             }
 
-            // add R2 elems
+            // add R2 elems and split existent R3 elems
             // 2023.08.24
 
             List<int> removeIds = new List<int>();
             var initialR3s = ss_ref.Elems.Where(e => e.Autorule == 3).ToList();
             foreach (SG_Elem1D r3e in initialR3s)
             {
+                var selNds = ss_ref.Nodes
+                    .Where(n => n.Pt.X > r3e.Nodes[0].Pt.X  && n.Pt.X  < r3e.Nodes[1].Pt.X)
+                    .Where(n => n.Elements.Where(e => e.Autorule == 1).Count() != 0)
+                    .OrderBy(n => n.Pt.X)
+                    .ToList();
 
-                var selNds = ss_ref.Nodes.Where(n => n.Pt.X > r3e.Nodes[0].Pt.X && n.Pt.X < r3e.Nodes[1].Pt.X).ToList();
-                foreach (var nd in selNds)
+                var lastNode = new SG_Node();
+                for (var i=0; i< selNds.Count();i++)
                 {
-                    // unselect irrelevant nodes
-                    if (nd.Elements.Where(e => e.Autorule == 1).Count() == 0) continue;
+                    var nd = selNds[i];
 
                     // find intersection
                     var vL = new Line(nd.Pt, Vector3d.ZAxis);
                     Intersection.LineLine(r3e.Ln, vL, out double param, out _);
 
                     // add intermediate node
-                    SG_Node midNode = SG_Node.CreateNode(r3e, param, ss_ref.nodeCount);
-                    ss_ref.Nodes.Add(midNode);
+                    SG_Node middle_nd = SG_Node.CreateNode(r3e, param, ss_ref.nodeCount);
+                    ss_ref.Nodes.Add(middle_nd);
                     ss_ref.nodeCount++;
 
+                    var left_nd = new SG_Node();
+                    var right_nd = new SG_Node();
+                    bool bl_left = false;
+                    bool bl_right = false;
+                    if (i == 0 && i == selNds.Count() - 1)
+                    {
+                        left_nd = r3e.Nodes[0];
+                        right_nd = r3e.Nodes[1];
+                        bl_left = true;
+                        bl_right = true;
+                    }
+                    else if (i == 0)
+                    {
+                        left_nd = r3e.Nodes[0];
+                        bl_left = true;
+                    }
+                    else if (i == selNds.Count() -1)
+                    {
+                        left_nd = lastNode;
+                        right_nd = r3e.Nodes[1];
+                        bl_left = true;
+                        bl_right = true;
+                    }
+                    else
+                    {
+                        left_nd = lastNode;
+                        bl_left = true;
+                    }
+
                     // create one vertical elem and two R3 elem by splitting the initial R3 elem
-                    SG_Elem1D newVL = new SG_Elem1D(new SG_Node[2] { nd, midNode }, ss_ref.elementCount, "AR2") { Autorule = 3 };
-                    SG_Elem1D newR3_0 = new SG_Elem1D(new SG_Node[2] { r3e.Nodes[0], midNode }, ss_ref.elementCount + 1, r3e.Name) { Autorule = 3 };
-                    SG_Elem1D newR3_1 = new SG_Elem1D(new SG_Node[2] { midNode, r3e.Nodes[1] }, ss_ref.elementCount + 2, r3e.Name) { Autorule = 3 };
+                    SG_Elem1D newVL = new SG_Elem1D(new SG_Node[2] { nd, middle_nd }, ss_ref.elementCount, "AR2") { Autorule = 3 };
+                    ss_ref.Elems.Add(newVL);
+                    ss_ref.elementCount++;
 
-                    ss_ref.elementCount += 3;
+                    if (bl_left)
+                    {
+                        SG_Elem1D newR3_0 = new SG_Elem1D(new SG_Node[2] { left_nd, middle_nd }, ss_ref.elementCount, r3e.Name) { Autorule = 3 };
+                        ss_ref.Elems.Add(newR3_0);
+                        ss_ref.elementCount++;
+                    }
 
-                    // remove element just split
-                    removeIds.Add(r3e.ID);
+                    if (bl_right)
+                    {
+                        SG_Elem1D newR3_1 = new SG_Elem1D(new SG_Node[2] { middle_nd, right_nd }, ss_ref.elementCount, r3e.Name) { Autorule = 3 };
+                        ss_ref.Elems.Add(newR3_1);
+                        ss_ref.elementCount++;
+                    }
 
-                    // add three elements to shape
-                    ss_ref.Elems.AddRange(new List<SG_Element>(3) { newVL, newR3_0, newR3_1 });
+                    // register this turn's midnode for the next use
+                    lastNode = middle_nd;
 
                 }
-
+                // remove element just split 
+                if (selNds.Count() != 0)
+                {
+                    removeIds.Add(r3e.ID);
+                }
+                
             }
 
             ss_ref.Elems = ss_ref.Elems.Where(e => removeIds.Contains(e.ID) == false).ToList();
